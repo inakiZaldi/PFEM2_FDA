fb606dee32e2f6cc8268b5105090a1c32c83a67c
fdelpin <fdelpin@gmail.com>
2019-01-28 16:24:15 -0800

    1) added scalability plot 2) added pfem-2 algorithm

---
+++ imgs/scal.m
@@ -0,0 +1,17 @@
+close all; clear all;
+h=figure();
+load scalability.dat
+sup=scalability(1,2)./scalability(:,2);
+sup1=scalability(1,3)./scalability(:,3);
+plot (scalability(:,1),scalability(:,1),"linewidth",3,'+-r',"markersize",13);
+hold on;
+plot (scalability(:,1),sup,"linewidth",3,'o-b',"markersize",13);
+plot (scalability(:,1),sup1,"linewidth",3,'x-k',"markersize",13);
+ylabel("Speed Up")
+xlabel("Number of CPUs")
+grid;
+legend("Ideal","Constant Num. of Part","No Particle removal","location","northwest")
+legend("boxon")
+print(h,'-dpng','-color','vib_plt.png')
+print -deps scalability.eps
+
